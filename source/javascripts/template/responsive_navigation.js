7d91f036e8798002cffd4c77d0ca67d3aee896af
Jaime Iniesta <jaimeiniesta@gmail.com>
2015-03-03 11:54:16 +0100

    adds responsive navigation

---
+++ source/javascripts/template/responsive_navigation.js
@@ -0,0 +1,37 @@
+// When DOM is fully loaded
+jQuery(document).ready(function($) {
+
+/* --------------------------------------------------------
+	Responsive navigation
+   --------------------------------------------------------	*/
+
+	(function() {
+
+		var $mainNav    = $('.navbar .nav'),
+			responsiveNav = '<option value="" selected>Navigate...</option>';
+
+		// Responsive nav
+		$mainNav.find('li').each(function() {
+			var $this   = $(this),
+				$link = $this.children('a'),
+				depth   = $this.parents('ul').length - 1,
+				indent  = '';
+
+			if( depth ) {
+				while( depth > 0 ) {
+					indent += ' - ';
+					depth--;
+				}
+			}
+
+			if ($link.text())
+				responsiveNav += '<option ' + ($this.hasClass('active') ? 'selected="selected"':'') + ' value="' + $link.attr('href') + '">' + indent + ' ' + $link.text() + '</option>';
+
+		}).end().after('<select class="nav-responsive">' + responsiveNav + '</select>');
+
+		$('.nav-responsive').on('change', function() {
+			window.location = $(this).val();
+		});
+
+	})();
+})
