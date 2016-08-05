9304334de62d05691bbd89b581613aa90dc74d88
Jaime Iniesta <jaimeiniesta@gmail.com>
2016-08-05 20:20:53 +0200

    adds Rocket Validator banner

---
+++ source/javascripts/app.js
@@ -0,0 +1,5 @@
+$(function() {
+  noty({text: "Check out our new <strong><a href='https://rocketvalidator.com/'>Rocket Validator</a></strong> for unlimited HTML validations at a blazing speed!",
+                theme: "relax",
+                layout: "topRight"});
+})
