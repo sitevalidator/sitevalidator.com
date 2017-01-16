f3bc6c0f28892b3fe675260dc7d1ba20332326a3
Jaime Iniesta <jaimeiniesta@gmail.com>
2017-01-16 23:31:22 +0100

    disable noty

--- source/javascripts/app.js
+++
@@ -1,5 +0,0 @@
-$(function() {
-  noty({text: "Check out our new <strong><a href='https://rocketvalidator.com/'>Rocket Validator</a></strong> for unlimited HTML validations at a blazing speed!",
-                theme: "relax",
-                layout: "topRight"});
-})
